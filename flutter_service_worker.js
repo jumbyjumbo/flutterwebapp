dc52be07b2111826e938e4fea7085666b1a8595b
zaksorel <sorelzakary@gmail.com>
2023-05-26 11:41:22 -0400

    Built web
    Branch: main
    Commit: ab96169880f143867fc41b064ed0fdcca5537e7d (dirty)
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "980547175e325fe622a3362b84d55b6a",
-"index.html": "b52b839f23d91905c249b96f7b359668",
-"/": "b52b839f23d91905c249b96f7b359668",
-"main.dart.js": "9c45af0699d8149677d94090a423925f",
+"index.html": "1d6b74b7bddd249d4b9bccf9ec7462c0",
+"/": "1d6b74b7bddd249d4b9bccf9ec7462c0",
+"main.dart.js": "d90d3d352b43f634352e0fd20c096c23",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
